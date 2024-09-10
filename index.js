2ee2278492ec6e87bc708cc1734359cb1fb41a0a
Davi Camargo <davi.assis@aluno.ifsp.edu.br>
2024-09-09 21:46:57 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const sim = window.document.getElementById("opcyes")
+const nao = window.document.getElementById("opcNo")
+const fundo = window.document.getElementById("area")
+const resp = window.document.getElementById("textinho")
+
+nao.addEventListener("mouseenter", entrar)
+let estado = -1
+
+function entrar(){
+    estado++
+    if (estado == 0){
+        nao.style.transform="translate(13px, -15px)"
+    }
+    if (estado == 1){
+        nao.style.transform="translate(13px, +15px)"
+    }
+    if (estado == 2){
+        nao.style.transform="translate(-32px, +15px)"
+    }
+    if (estado == 3){
+        nao.style.transform="translate(-32px, -15px)"
+    }
+    if (estado == 4){
+        nao.style.transform="translate(+13px, +15px)"
+    }
+    if (estado == 5){
+        nao.style.transform="translate(+13px, -15px)"
+    }
+    if (estado == 6){
+        nao.style.transform="translate(-32px, +15px)"
+    }
+    if (estado == 7){
+        nao.style.transform="translate(-32px, -15px)"
+        estado = -1
+    }
+}
+
+sim.addEventListener("click", clicar)
+
+function clicar(){
+    resp.innerHTML ="Eu te amo meu amoooor!!"
+    sim.style.transform="translate(0px)"
+    nao.style.transform="translate(0px, 0px)"
+
+    resp.style.opacity= 1;
+
+    setTimeout(function(){
+        resp.style.opacity = 0;
+      }, 3000 )
+    
+}
